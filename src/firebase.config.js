6034eacf41dd3fb6136f0c1c11a45819a93d78ad
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-10-13 11:58:13 +0600

    firebase installed

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCP9F0CrkxcieKxAovCP3dnRymF4wy8GZw",
+    authDomain: "simple-firebase-app-f7532.firebaseapp.com",
+    projectId: "simple-firebase-app-f7532",
+    storageBucket: "simple-firebase-app-f7532.appspot.com",
+    messagingSenderId: "1098636013810",
+    appId: "1:1098636013810:web:417fa9bccca50013e3606b"
+  };
+
+  export default firebaseConfig;
