800f30a7696663a8733898e1fe234300a67ee0c4
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-10-14 10:00:24 +0600

    google sign in authentication created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 
 import './App.css';
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
-firebase.initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 function App() {
+
+  const provider = new GoogleAuthProvider();
+  const [ user, setUser ] = useState({
+    isSignedIn : false,
+    name : '',
+    emailAddress : '',
+    userImage : ''
+  });
+
+  const handleSignIn = () =>{
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+    .then(res => {
+      const { displayName, email, photoURL} = res.user;
+      const signedInUser = {
+        isSignedIn : true,
+        name : displayName,
+        emailAddress : email,
+        userImage : photoURL
+      }
+      setUser(signedInUser)
+    })
+    .catch( err => console.log(err))
+  }
+
   return (
     <div className="App">
-
+      <button onClick={handleSignIn}>Sign In </button>
+      {
+        user.isSignedIn && <div>
+            <p>Welcome <b> { user.name } </b></p>
+            <p>Your email : { user.emailAddress} </p>
+            <img src={user.userImage} alt="" />
+          </div>
+      }
     </div>
   );
 }
